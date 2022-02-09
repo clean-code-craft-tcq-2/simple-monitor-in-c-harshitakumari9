2257c4e870ed047853d78c82f01f04f4f749b22f
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-02-09 23:45:19 +0530

    Update and rename checker.h to check_limits.h

---
+++ check_limits.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define Low_temp_limit         (float)0U
+#define High_temp_limit        (float)45U
+
+#define soc_lower_limit        (float)20U
+#define soc_upper_limit        (float)80U
+
+#define chargeRate_threshold   (float)0.8
+
+int (*Check_Temperature_Range)(float);
+int (*Check_SOC_Range)(float);
+int (*Charge_Rate_Limit)(float);
