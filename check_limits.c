a0b8fd2d5a9983a485ad0f7b1f97d6266318edd0
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-02-10 08:52:16 +0530

    Update check_limits.c

--- check_limits.c
+++ check_limits.c
@@ -4,7 +4,7 @@
 
 int Check_Temperature_Range(float temp_in_celcius)
 {
-  if((temp_in_celcius <= Low_temp_limit) || (temp_in_celcius => High_temp_limit)) 
+  if((temp_in_celcius <= Low_temp_limit) || (temp_in_celcius >= High_temp_limit)) 
   {
     printf("Temperature out of range!\n");
     return 0;
@@ -15,7 +15,7 @@ int Check_Temperature_Range(float temp_in_celcius)
 
 int Check_SOC_Range(float soc_value)
 {
-  if((soc_value <= soc_lower_limit) || (temp_in_celcius => soc_upper_limit)) 
+  if((soc_value <= soc_lower_limit) || (soc_value => soc_upper_limit)) 
   {
     printf("State of Charge out of range!\n");
     return 0;
