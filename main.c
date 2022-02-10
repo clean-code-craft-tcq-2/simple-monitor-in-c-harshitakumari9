6abe20274b7f84838b3cee6a718555e9e8859298
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-02-10 09:39:11 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 
 int main() 
 {
-  int (*fnPtrForCheckBatteryStatus)(float,float,float) = batteryIsOk;
+  int (*fnPtrForCheckBatteryStatus)(float,float,float) = test_battery_status;
   
-  test_battery_status(1, 45, 63, 0.5);
-  test_battery_status(0, 54, 90, 0.9);
+  test_battery_status(1, 45, 63, 0.5, fnPtrForCheckBatteryStatus);
+  test_battery_status(0, 54, 90, 0.9, fnPtrForCheckBatteryStatus);
+  
+  return 0;
 }
