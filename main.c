66b67bc5a6d0b7a258dcc5060c7c16a787c5c52b
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-02-10 08:32:41 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "check_limits.h"
 
-int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+int main() 
+{
+  test_battery_status(1, 45, 63, 0.5);
+  test_battery_status(0, 54, 90, 0.9);
 }
